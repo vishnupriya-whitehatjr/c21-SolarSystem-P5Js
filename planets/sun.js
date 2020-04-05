25a57960d484b44ca728b2b0a04b5099ac71d306
vishnupriya-whitehatjr <whitehatjr.priya@gmail.com>
2020-04-05 23:35:38 +0530

    commit

--- planets/sun.js
+++ planets/sun.js
@@ -1,8 +1,10 @@
 class Sun{
     constructor(x,y,radius){
+        this.body=Bodies.circle(x,y,radius);
         this.x=x;
         this.y=y;
         this.radius=radius;
+        this.image = loadImage("images/sun.png");
     }
     display(){
         ellipseMode(CENTER);
--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 var sun, mercury;
 
 function setup() {
